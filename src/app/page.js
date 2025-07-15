57c38abc380dbbf034ed6fe3911ede9503bd7f62
nafirar <danisfira.nr@gmail.com>
2025-07-15 16:08:09 +0700

    Add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import InvesmentIntro from "@/components/InvesmentIntro";
 import FinancialDimensions from "@/components/FinancialDimensions";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <FinancialDimensions />
       <InvesmentIntro />
+      <Footer/>
     </>
   );
 }
