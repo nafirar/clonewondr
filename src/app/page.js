0e8125da47b4c85d6cd482dca839449a00c13c40
nabilahluthfia <nabilahluthfianasari@gmail.com>
2025-07-15 16:03:46 +0700

    docs: uninstall babel

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar';
-import Carousel from '../components/Carousel';
 import SimpleCarousel from '../components/Carousel';
+
 
 export default function Home() {
   return (
