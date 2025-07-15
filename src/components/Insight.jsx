3bf7d50652d46f751b583473da83813711b9783e
dimasdytya <89495814+dimasdytya@users.noreply.github.com>
2025-07-15 15:05:27 +0700

    ini insight

---
+++ src/components/Insight.jsx
@@ -0,0 +1,27 @@
+export default function Investasi() {
+  return (
+    <section className="py-6 xl:px-[200px] md:pt-9 md:pb-[72px] bg-white">
+      <div className="px-6">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[56px] !leading-snug lg:!leading-tight text-black text-center font-semibold">
+          Insight cermat dari kebiasaan keuangan
+        </h2>
+      </div>
+      <div className="pl-6">
+        <div className="w-full mt-6 md:mt-12 overflow-x-auto md:overflow-x-hidden">
+          <div className="min-w-[600px]">
+            <img
+              alt="insight"
+              loading="lazy"
+              width="600"
+              height="400"
+              decoding="async"
+              className="w-full h-auto pr-6 md:pr-0"
+              src="https://wondr.bni.co.id/api/image/content_1751486086818.png"
+              style={{ color: "transparent" }}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
