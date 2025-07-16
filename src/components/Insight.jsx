9afd9ec5dc00157b345b4663d6b5dd11bf9cdc5f
dimasdytya <89495814+dimasdytya@users.noreply.github.com>
2025-07-16 10:56:21 +0700

    update responsif di components insight

--- src/components/Insight.jsx
+++ src/components/Insight.jsx
@@ -1,25 +1,22 @@
 export default function Insight() {
   return (
-    <section className="py-6 xl:px-[200px] md:pt-9 md:pb-[72px] bg-white">
-      <div className="px-6">
-        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[56px] !leading-snug lg:!leading-tight text-black text-center font-semibold">
+    <section className="py-6 md:py-9 xl:py-[72px] px-4 md:px-8 xl:px-[200px] bg-white">
+      <div className="text-center">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[56px] leading-snug lg:leading-tight text-black font-semibold">
           Insight cermat dari kebiasaan keuangan
         </h2>
       </div>
-      <div className="pl-6">
-        <div className="w-full mt-6 md:mt-12 overflow-x-auto md:overflow-x-hidden">
-          <div className="min-w-[600px]">
-            <img
-              alt="insight"
-              loading="lazy"
-              width="600"
-              height="400"
-              decoding="async"
-              className="w-full h-auto pr-6 md:pr-0"
-              src="https://wondr.bni.co.id/api/image/content_1751486086818.png"
-              style={{ color: "transparent" }}
-            />
-          </div>
+      <div className="mt-6 md:mt-12 flex justify-center">
+        <div className="w-full max-w-[600px]">
+          <img
+            alt="insight"
+            loading="lazy"
+            width="600"
+            height="400"
+            decoding="async"
+            className="w-full h-auto rounded-lg"
+            src="https://wondr.bni.co.id/api/image/content_1751486086818.png"
+          />
         </div>
       </div>
     </section>
